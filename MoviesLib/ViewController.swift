e5df138e405d23e95517c8833f89a20d89631bb1
Bruno Henrique <brunohosilva97@gmail.com>
2022-08-30 19:17:58 -0300

    Initial Commit

---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Bruno Oliveira on 30/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
